0e6e55abe9b2404d783d5abf5202ccf97eb9d1de
nobuh <nobu.hatano@gmail.com>
2026-06-27 15:42:00 +0900

    wc: read from stdin

--- wc.go
+++ wc.go
@@ -15,97 +15,93 @@ func Wc(args []string, cOpt *bool, lOpt *bool, mOpt *bool, wOpt *bool) {
         *cOpt, *lOpt, *wOpt = true, true, true
     }
 
-    if len(args) > 0 {
-        if *mOpt {
-            // multibyte char count
-            file, err := os.Open(args[0])
-            if err != nil {
-                log.Fatalf("open fail: %v", err)
-            }
-            defer file.Close()
+    var reader *bufio.Reader
+    var filename string
 
-            reader := bufio.NewReader(file)
-            count := 0
-            for {
-                _, _, err := reader.ReadRune()
-                if err != nil {
-                    if err == io.EOF {
-                        // finish reading
-                        break
-                    }
-                    log.Fatalf("read error: %v", err)
-                    return
+    if len(args) > 0 && args[0] != "-" {
+        // input file name is given and the file is not "-"
+        filename = args[0]
+        file, err := os.Open(filename)
+        reader = bufio.NewReader(file)
+        if err != nil {
+            log.Fatalf("open fail: %v", err)
+        }
+        defer file.Close()
+    } else {
+        // input file name is "-" or stdin
+        filename = ""
+        reader = bufio.NewReader(os.Stdin)
+    }
+
+    if *mOpt {
+        count := 0
+        for {
+            _, _, err := reader.ReadRune()
+            if err != nil {
+                if err == io.EOF {
+                    // finish reading
+                    break
                 }
-                count++
-            }
-
-            fmt.Printf("%d ", count)
-            fmt.Println(args[0])
-            return
-        } else {
-            // count byte, line, word or all of them
-            file, err := os.Open(args[0])
-            if err != nil {
-                log.Fatalf("open fail: %v", err)
+                log.Fatalf("read error: %v", err)
                 return
             }
-            defer file.Close()
+            count++
+        }
 
-            reader := bufio.NewReader(file)
-            nchar, nline, nword := 0, 0, 0      
-            onWord := false
-
-            for {
-                b, err := reader.ReadByte()
-                if err != nil {
-                    if err == io.EOF {
-                        // finish reading
-                        break
-                    }
-                    log.Fatalf("read error: %v", err)
-                    return
-                }
-
-                if *cOpt {
-                    nchar++
-                }
-
-                if *lOpt && b == '\n' {
-                    nline++
-                }
-
-                if *wOpt {
-                    if !onWord && !unicode.IsSpace(rune(b)) {
-                        // head of the word
-                        onWord = true
-                        nword++
-                    } else if onWord && unicode.IsSpace(rune(b)) {
-                        // tail of the word
-                        onWord = false
-                    } 
-                }
-            }
-
-            // If the original wc with l, w and c option, 
-            // the output format is starting white space and its different
-            // from the case only single option has selected.
-            // However it must be useless.
-            
-            if *lOpt {
-                fmt.Printf("%d ", nline)
-            }
-            if *wOpt {
-                fmt.Printf("%d ", nword)
-            }
-            if *cOpt {
-                fmt.Printf("%d ", nchar)
-            }
-            fmt.Println(args[0])
-            return
-        }  // c, l, w option
+        fmt.Printf("%d ", count)
+        fmt.Println(filename)
+        return
     } else {
-        // no input file
-        log.Fatalf("need a input file.")
-    }        
+        nchar, nline, nword := 0, 0, 0      
+        onWord := false
+
+        for {
+            b, err := reader.ReadByte()
+            if err != nil {
+                if err == io.EOF {
+                    // finish reading
+                    break
+                }
+                log.Fatalf("read error: %v", err)
+                return
+            }
+
+            if *cOpt {
+                nchar++
+            }
+
+            if *lOpt && b == '\n' {
+                nline++
+            }
+
+            if *wOpt {
+                if !onWord && !unicode.IsSpace(rune(b)) {
+                    // head of the word
+                    onWord = true
+                    nword++
+                } else if onWord && unicode.IsSpace(rune(b)) {
+                    // tail of the word
+                    onWord = false
+                } 
+            }
+        }
+
+        // If the original wc with l, w and c option, 
+        // the output format is starting white space and its different
+        // from the case only single option has selected.
+        // However it must be useless.
+        
+        if *lOpt {
+            fmt.Printf("%d ", nline)
+        }
+        if *wOpt {
+            fmt.Printf("%d ", nword)
+        }
+        if *cOpt {
+            fmt.Printf("%d ", nchar)
+        }
+        fmt.Println(filename)
+        return
+    }  // c, l, w option
 }
 
